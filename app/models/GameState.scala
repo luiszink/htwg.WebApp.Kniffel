4335620c65ee43eff16b39ce2add0e622a45afbd
luiszink <157110320+luiszink@users.noreply.github.com>
2024-10-22 08:19:11 +0200

    working html

--- app/models/GameState.scala
+++ app/models/GameState.scala
@@ -1,3 +1,5 @@
 package models
 
-case class GameState(output: String, players: List[String], categories: List[String])
+import de.htwg.se.kniffel.model.PlayerInterface
+
+case class GameState(output: String, players: List[PlayerInterface], categories: List[String])
